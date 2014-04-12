ddad48baa5cfe95fe1fc31cf19bbebd2a2b43fc3
Stefan Haflidason <styrmis@gmail.com>
2014-04-12 21:10:03 +0100

    'copyright' field should be datetime, not string

--- Chapter-4/CoreDataBooks/schemas/0001_initial.rb
+++ Chapter-4/CoreDataBooks/schemas/0001_initial.rb
@@ -3,6 +3,6 @@ schema "0001 initial" do
   entity "Book" do
     string :title, optional: false
     string :author, optional: false
-    string :copyright, optional: true
+    datetime :copyright, optional: true
   end
 end
