1cf1a773421e16ab3abbc5c8b11c9555d44c9a6f
Stefan Haflidason <styrmis@gmail.com>
2014-10-08 14:55:55 +0900

    Set up Person entity and Core Data stack with CDQ

--- Chapter-6/SimpleHeavyweightMigration/app/app_delegate.rb
+++ Chapter-6/SimpleHeavyweightMigration/app/app_delegate.rb
@@ -1,5 +1,8 @@
 class AppDelegate
+  include CDQ
+
   def application(application, didFinishLaunchingWithOptions:launchOptions)
+    cdq.init
     true
   end
 end
---
+++ Chapter-6/SimpleHeavyweightMigration/schemas/0001_initial.rb
@@ -0,0 +1,6 @@
+schema "0001 initial" do
+  entity "Person" do
+    string :name, optional: false
+    string :address, optional: false
+  end
+end
